d68321c8d081d676effb5abbc9d905141ee7cbdd
jiheunkim <ziheun54@gmail.com>
2023-05-26 07:06:58 +0900

    맛지도 툴바 2차

--- app/src/main/java/com/example/bobmukjaku/MapListFragment.kt
+++ app/src/main/java/com/example/bobmukjaku/MapListFragment.kt
@@ -1,12 +1,10 @@
 package com.example.bobmukjaku
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.bobmukjaku.databinding.FragmentMapBinding
-
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,35 +13,28 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MapFragment.newInstance] factory method to
+ * Use the [MapListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MapFragment : Fragment() {
+class MapListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-
-    lateinit var binding: FragmentMapBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
-        setupToolbar()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMapBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    private fun setupToolbar() {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map_list, container, false)
     }
 
     companion object {
@@ -53,12 +44,12 @@ class MapFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MapFragment.
+         * @return A new instance of fragment MapListFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MapFragment().apply {
+            MapListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
