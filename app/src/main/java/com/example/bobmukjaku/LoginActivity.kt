abff7ce4cb24c9682d266e3f3eae22a3e4901a65
jiheunkim <ziheun54@gmail.com>
2023-05-27 00:47:49 +0900

    아이디 비밀번호 공백 입력 시 앱 꺼지는 오류 해결

--- app/src/main/java/com/example/bobmukjaku/LoginActivity.kt
+++ app/src/main/java/com/example/bobmukjaku/LoginActivity.kt
@@ -24,25 +24,34 @@ class LoginActivity : AppCompatActivity() {
     private fun initLayout() {
         binding.apply {
             loginBtn.setOnClickListener {
-                val email = loginId.text.toString()
+                val id = loginId.text.toString()
                 val passwd = loginPasswd.text.toString()
 
                 auth = Firebase.auth
-                auth.signInWithEmailAndPassword(email, passwd)
-                    .addOnCompleteListener(this@LoginActivity) { task ->
-                        if (task.isSuccessful) {
-                            // Sign in success, update UI with the signed-in user's information
 
-                            //데이터베이스에 user정보를 넣어줘야함
-                            val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                            startActivity(intent)
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            //updateUI(null)
-                            Toast.makeText(this@LoginActivity, "login 실패", Toast.LENGTH_LONG).show()
+                if (id.isEmpty() && passwd.isEmpty()) {
+                    Toast.makeText(this@LoginActivity, "아이디와 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                } else if (id.isEmpty()) {
+                    Toast.makeText(this@LoginActivity, "아이디를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                } else if (passwd.isEmpty()) {
+                    Toast.makeText(this@LoginActivity, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                } else {
+                    auth.signInWithEmailAndPassword(id, passwd)
+                        .addOnCompleteListener(this@LoginActivity) { task ->
+                            if (task.isSuccessful) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                //데이터베이스에 user정보를 넣어줘야함
+                                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                startActivity(intent)
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                //updateUI(null)
+                                Toast.makeText(this@LoginActivity, "login 실패", Toast.LENGTH_LONG).show()
+                            }
                         }
-                    }
+                }
             }
 
             joinBtn.setOnClickListener {
