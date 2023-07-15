17cb4bb3e25c6ec3f229b0819771c01d59358c62
jiheunkim <ziheun54@gmail.com>
2023-07-16 04:38:56 +0900

    밥친구 메뉴 하위탭 fragment 생성

--- app/src/main/java/com/example/bobmukjaku/FriendFragment.kt
+++ app/src/main/java/com/example/bobmukjaku/FriendFragment.kt
@@ -1,59 +1,71 @@
 package com.example.bobmukjaku
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.bobmukjaku.databinding.FragmentFriendBinding
+import com.google.android.material.tabs.TabLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FriendFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FriendFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentFriendBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend, container, false)
+        binding = FragmentFriendBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FriendFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FriendFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Toolbar()
+    }
+
+    private fun Toolbar() {
+        val tabLayout = binding.friendTabs
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val tabPosition = tab?.position
+
+                tab?.icon?.setColorFilter(Color.parseColor("#28A872"), PorterDuff.Mode.SRC_IN)
+
+                when (tabPosition) {
+                    0 -> {
+                        val friendListFragment = FriendListFragment()
+                        requireActivity().supportFragmentManager.beginTransaction()
+                            .replace(R.id.friend_container, friendListFragment).commit()
+                    }
+                    1 -> {
+                        val blockFragment = BlockFragment()
+                        requireActivity().supportFragmentManager.beginTransaction()
+                            .replace(R.id.friend_container, blockFragment).commit()
+                    }
                 }
             }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // icon color black으로
+                tab?.icon?.setColorFilter(Color.parseColor("#E0E0E0"), PorterDuff.Mode.SRC_IN)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                //구현하지 않음
+            }
+
+        })
+
+        // Set default fragment
+        val friendListFragment = FriendListFragment()
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.friend_container, friendListFragment).commit()
+
+        tabLayout.getTabAt(0)?.icon?.setColorFilter(Color.parseColor("#28A872"), PorterDuff.Mode.SRC_IN)
     }
 }
