549dd925f9f071b2e07458d98c3fbc852567151d
jiheunkim <ziheun54@gmail.com>
2023-07-30 00:20:10 +0900

    회원가입: 비밀번호 기능 수정, 회원가입 버튼 누를 시 db 업데이트

--- app/src/main/java/com/example/bobmukjaku/JoinActivity.kt
+++ app/src/main/java/com/example/bobmukjaku/JoinActivity.kt
@@ -76,13 +76,13 @@ class JoinActivity : AppCompatActivity() {
             binding.nickcheckbtn.setOnClickListener {
                 val nickname = binding.username.text.toString().trim()
 
-                if (nickname.isEmpty()) {
-                    binding.nickConfirm.text = "닉네임을 입력해주세요."
+                if (nickname.length < 2) {
+                    binding.nickConfirm.text = "닉네임은 최소 2자 이상 입력해주세요."
                     binding.nickConfirm.setTextColor(ContextCompat.getColor(applicationContext, R.color.red))
                 } else {
                     // 닉네임 중복 확인
                     usernameFromMariaDB(nickname) { isAvailable ->
-                        if (isAvailable) {
+                        if (!isAvailable) {
                             // 중복되지 않은 경우
                             binding.nickConfirm.text = "사용가능한 닉네임입니다."
                             binding.nickConfirm.setTextColor(ContextCompat.getColor(applicationContext, R.color.black))
@@ -95,73 +95,114 @@ class JoinActivity : AppCompatActivity() {
                 }
             }
 
-            if (binding.nickConfirm.text == "사용가능한 닉네임입니다.") {
-                binding.username.addTextChangedListener {
+            binding.username.addTextChangedListener {
+                if (binding.nickConfirm.text == "사용가능한 닉네임입니다.") {
                     // 사용자가 닉네임을 수정하면 중복확인을 다시 하도록 상태 변경
                     binding.nickConfirm.text = "닉네임 중복확인을 다시 해주세요."
                     binding.nickConfirm.setTextColor(ContextCompat.getColor(applicationContext, R.color.red))
                 }
             }
 
-            passwdAreaCheck.addTextChangedListener(object : TextWatcher {
+            // 비밀번호 조건
+            val passwordPattern = "^(?=.*[!@^&,.?])(?=.*[A-Za-z]).{8,15}$".toRegex()
+
+            passwdArea.addTextChangedListener(object : TextWatcher {
                 //입력이 끝났을 때
-                // 비밀번호 일치하는지 확인
-                override fun afterTextChanged(p0: Editable?) {
+                override fun afterTextChanged(editable: Editable?) {
                 }
                 //입력하기 전
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 }
                 //텍스트 변화가 있을 시
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    if(passwdArea.text.toString() == passwdAreaCheck.text.toString()){
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    val passwd = passwdArea.text.toString()
+
+                    // 비밀번호 조건 검사
+                    if (passwd.length in 8..15 && passwd.matches(passwordPattern)) {
+                        pwInfo.text = "해당 비밀번호는 사용 가능합니다."
+                        pwInfo.setTextColor(ContextCompat.getColor(applicationContext, R.color.black))
+                    } else {
+                        pwInfo.text = "비밀번호는 8~15자이며, 다음 특수문자('!', '@', '^', '&', ',', '.', '?') 중 하나 이상을 포함시켜야 합니다.)"
+                        pwInfo.setTextColor(ContextCompat.getColor(applicationContext, R.color.red))
+                        if (pwConfirm.text == "비밀번호가 일치합니다.") {
+                            pwConfirm.text = ""
+                            // 가입하기 버튼 비활성화
+                            joinButton.isEnabled = false
+                        }
+                    }
+                }
+            })
+
+            passwdAreaCheck.addTextChangedListener(object : TextWatcher {
+                //입력이 끝났을 때
+                override fun afterTextChanged(editable: Editable?) {
+                }
+                //입력하기 전
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+                //텍스트 변화가 있을 시
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    val passwdCheck = passwdAreaCheck.text.toString()
+                    val passwd = passwdArea.text.toString()
+
+                    if (passwdCheck == passwd) {
                         pwConfirm.text = "비밀번호가 일치합니다."
+                        pwConfirm.setTextColor(ContextCompat.getColor(applicationContext, R.color.black))
 
                         // 가입하기 버튼 활성화
-                        joinButton.isEnabled=true
-                    }
-                    else{
+                        joinButton.isEnabled = true
+                    } else {
                         pwConfirm.text = "비밀번호가 일치하지 않습니다."
-                        pwConfirm.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.red))
+                        pwConfirm.setTextColor(ContextCompat.getColor(applicationContext, R.color.red))
+
+                        // 가입하기 버튼 비활성화
+                        joinButton.isEnabled = false
                     }
                 }
             })
 
             //회원가입 버튼 누를 경우
             joinButton.setOnClickListener {
-                //입력한 passwd를 가져와서 변수에 저장
-                val passwd = passwdArea.text.toString()
+                if (binding.nickConfirm.text == "사용가능한 닉네임입니다.") {
+                    if (pwInfo.text == "해당 비밀번호는 사용 가능합니다.") {
+                        if (pwConfirm.text == "비밀번호가 일치합니다.") {
+                            //입력한 닉네임, passwd를 가져와서 변수에 저장
+                            val nickname = username.text.toString()
+                            val passwd = passwdArea.text.toString()
 
-                //인증 데이터베이스(로그인용)에 사용자 추가
-                auth = Firebase.auth
-                if (email != null) {
-                    auth.createUserWithEmailAndPassword(email, passwd)
-                        .addOnCompleteListener(this@JoinActivity) { task ->
-                            if (task.isSuccessful) {
-                                //추가 성공할 경우
+                            // 사용자 정보를 담는 Member 객체 생성
+                            val member = Member(memberEmail = email, memberNickName = nickname, memberPassword = passwd, certificatedAt = "", profileColor = "bg1", rate = 45, uid=0)
 
-                                val uid = FirebaseAuth.getInstance().uid ?: null
-                                val username = username.text.toString()
-                                val user = User(uid!!, username)
+                            // MemberService의 insertMember 메서드를 호출하여 사용자 정보를 DB에 저장
+                            val call = RetrofitClient.memberService.insertMember(member)
 
-                                //데이터베이스에 회원가입한 사용자 정보 추가
-                                val table = Firebase.database.getReference("users")
-                                val tuple = table.child(uid)
-                                tuple.setValue(user).addOnCompleteListener{
-                                    Toast.makeText(this@JoinActivity, "객체넣기 성공", Toast.LENGTH_LONG).show()
-                                }
-                                    .addOnFailureListener {
-                                        Toast.makeText(this@JoinActivity, "객체넣기 실패", Toast.LENGTH_LONG).show()
+                            // 네트워크 요청을 비동기적으로 실행하도록 호출
+                            call.enqueue(object : Callback<Void> {
+                                override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                                    if (response.isSuccessful) {
+                                        // 데이터 삽입 성공
+                                        Toast.makeText(this@JoinActivity, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                                        val intent = Intent(this@JoinActivity, LoginActivity::class.java)
+                                        startActivity(intent)
+                                    } else {
+                                        // 데이터 삽입 실패
+                                        Toast.makeText(this@JoinActivity, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
                                     }
+                                }
 
-                                //재학생 인증 화면으로 전환
-                                val intent = Intent(this@JoinActivity, Join2Activity::class.java)
-                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                startActivity(intent)
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.d("LOGIN", "실패")
-                            }
+                                override fun onFailure(call: Call<Void>, t: Throwable) {
+                                    // 네트워크 오류 처리
+                                    Toast.makeText(this@JoinActivity, "네트워크 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                                }
+                            })
+                        } else {
+                            Toast.makeText(this@JoinActivity, "비밀번호 확인을 다시 해주세요.", Toast.LENGTH_SHORT)
                         }
+                    } else {
+                        Toast.makeText(this@JoinActivity, "비밀번호를 다시 입력해주세요.", Toast.LENGTH_SHORT)
+                    }
+                } else {
+                    Toast.makeText(this@JoinActivity, "닉네임을 다시 입력해주세요.", Toast.LENGTH_SHORT)
                 }
             }
         }
