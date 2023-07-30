c85dda49ad5b0234f964cebfd31bd2c22bea448b
jiheunkim <ziheun54@gmail.com>
2023-07-30 17:56:20 +0900

    모집방: 모집방 개설 버튼 화면전환

--- app/src/main/java/com/example/bobmukjaku/ChatFragment.kt
+++ app/src/main/java/com/example/bobmukjaku/ChatFragment.kt
@@ -15,20 +15,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChatFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     lateinit var mContext: Context
     lateinit var binding: FragmentChatBinding
@@ -40,22 +27,27 @@ class ChatFragment : Fragment() {
         mContext = context
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
-        getChatRoomList()
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentChatBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getChatRoomList()
+        makeChatRoom()
+    }
+
+    private fun makeChatRoom() {
+        // 모집방 개설 버튼 클릭 이벤트 처리
+        binding.openRoomBtn.setOnClickListener {
+            val intent = Intent(requireContext(), MakeRoomActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun getChatRoomList() {
@@ -81,26 +73,6 @@ class ChatFragment : Fragment() {
 
                 }
                 binding.joinRecyclerView.adapter = adapter
-            }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChatFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
             }
     }
 }
