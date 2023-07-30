5110cececa6bcba0cf23a9f02385c95d8cba9003
jiheunkim <ziheun54@gmail.com>
2023-07-30 17:55:50 +0900

    모집방개설: 닫기 버튼 화면 전환

--- app/src/main/java/com/example/bobmukjaku/MakeRoomActivity.kt
+++ app/src/main/java/com/example/bobmukjaku/MakeRoomActivity.kt
@@ -1,11 +1,27 @@
 package com.example.bobmukjaku
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bobmukjaku.databinding.ActivityMakeRoomBinding
 
 class MakeRoomActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMakeRoomBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_make_room)
+        binding = ActivityMakeRoomBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initLayout()
+    }
+
+    private fun initLayout() {
+        // 모집방 개설 닫기 클릭 이벤트 처리
+        binding.cancelBtn.setOnClickListener {
+            val intent = Intent(this@MakeRoomActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
