53fc406ac04622cb2b56735f9b40c5435f4995f4
jiheunkim <ziheun54@gmail.com>
2023-07-30 19:58:55 +0900

    모집방 개설: 날짜 기본 다이얼로그 넣어둠

--- app/src/main/java/com/example/bobmukjaku/MakeRoomActivity.kt
+++ app/src/main/java/com/example/bobmukjaku/MakeRoomActivity.kt
@@ -1,11 +1,18 @@
 package com.example.bobmukjaku
 
+import android.app.DatePickerDialog
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
 import com.example.bobmukjaku.databinding.ActivityMakeRoomBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MakeRoomActivity : AppCompatActivity() {
 
@@ -39,6 +46,58 @@ class MakeRoomActivity : AppCompatActivity() {
             val intent = Intent(this@MakeRoomActivity, MainActivity::class.java)
             startActivity(intent)
         }
+
+        // date_area에 현재 날짜 정보 설정
+        val currentDate = getCurrentDate()
+        binding.dateArea.setText(currentDate)
+
+        // date_area 클릭 이벤트 처리
+        binding.dateArea.setOnClickListener {
+            showDatePicker()
+        }
+    }
+
+    // 현재 날짜를 가져오는 함수
+    private fun getCurrentDate(): String {
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("yyyy년 M월 d일", Locale.getDefault())
+        return dateFormat.format(calendar.time)
+    }
+
+    // DatePicker 다이얼로그를 띄우는 함수
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // 현재 날짜 기준 2주 뒤까지 날짜 선택 가능
+        calendar.add(Calendar.WEEK_OF_YEAR, 2)
+        val maxDate = calendar.timeInMillis
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+                val selectedDate = formatDate(year, month, dayOfMonth)
+                binding.dateArea.text = selectedDate
+            },
+            year,
+            month,
+            day
+        )
+
+        datePickerDialog.datePicker.minDate = Calendar.getInstance().timeInMillis
+        datePickerDialog.datePicker.maxDate = maxDate
+        datePickerDialog.show()
+    }
+
+    // 날짜를 원하는 포맷으로 변환하는 함수
+    private fun formatDate(year: Int, month: Int, dayOfMonth: Int): String {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+        val dateFormat = SimpleDateFormat("yyyy년 M월 d일", Locale.getDefault())
+        return dateFormat.format(calendar.time)
     }
 
     // 음식 종류 버튼 클릭 시 호출되는 함수
