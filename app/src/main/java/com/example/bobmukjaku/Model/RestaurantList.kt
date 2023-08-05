3091397d29ed8be75f72bf99ec49acb4324958ff
jiheunkim <ziheun54@gmail.com>
2023-08-06 03:53:48 +0900

    맛지도: 식당 data class

---
+++ app/src/main/java/com/example/bobmukjaku/Model/RestaurantList.kt
@@ -0,0 +1,10 @@
+package com.example.bobmukjaku.Model
+
+data class RestaurantList(
+    var bizesNm: String,
+    var indsMclsNm: String,
+    var indsSclsNm: String,
+    var lnoAdr: String,
+    var lat: Double,
+    var lon: Double
+)
