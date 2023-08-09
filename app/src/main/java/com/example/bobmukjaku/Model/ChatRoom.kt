363b8b9c2e04078b1892122dd87865e8c61f0416
jiheunkim <ziheun54@gmail.com>
2023-08-09 21:13:08 +0900

    모집방DB: ChatRoom API 참고로 생성

---
+++ app/src/main/java/com/example/bobmukjaku/Model/ChatRoom.kt
@@ -0,0 +1,12 @@
+package com.example.bobmukjaku.Model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class ChatRoom(
+    @JsonProperty("roomName")var roomName: String?,
+    @JsonProperty("date")var date: String?,
+    @JsonProperty("startTime")var startTime: String?,
+    @JsonProperty("endTime")var endTime: String?,
+    @JsonProperty("kindOfFood")var kindOfFood: String?,
+    @JsonProperty("total")var total: Int?,
+)
