64332316fda0688a7a79a48fcb609b7b8eeb4275
jiheunkim <ziheun54@gmail.com>
2023-08-10 00:46:05 +0900

    ChatRoom 생성

--- app/src/main/java/com/example/bobmukjaku/Model/ChatRoom.kt
+++ app/src/main/java/com/example/bobmukjaku/Model/ChatRoom.kt
@@ -3,8 +3,9 @@ package com.example.bobmukjaku.Model
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class ChatRoom(
+    @JsonProperty("roomId") var roomId: Long?,
     @JsonProperty("roomName") var roomName: String?,
-    @JsonProperty("date") var date: String?,
+    @JsonProperty("meetingDate") var meetingDate: String?,
     @JsonProperty("startTime") var startTime: String?,
     @JsonProperty("endTime") var endTime: String?,
     @JsonProperty("kindOfFood") var kindOfFood: String?,
