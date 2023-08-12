c5509a2a4489798219c7390d5a2cfa896e3ab78b
jiheunkim <ziheun54@gmail.com>
2023-08-12 14:50:22 +0900

    비밀번호재설정: 레이아웃

---
+++ app/src/main/java/com/example/bobmukjaku/ChangePasswordctivity2.kt
@@ -0,0 +1,11 @@
+package com.example.bobmukjaku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangePasswordctivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_passwordctivity2)
+    }
+}
