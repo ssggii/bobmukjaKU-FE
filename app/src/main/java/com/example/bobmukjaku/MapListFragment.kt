39769c043d5ea5a45afd9eccd8c495e12650cf0d
jiheunkim <ziheun54@gmail.com>
2023-08-22 04:07:36 +0900

    맛지도: api 데이터 가져오고 저장하는 구조로 변경(초기 이후엔 저장된 데이터 재사용)

--- app/src/main/java/com/example/bobmukjaku/MapListFragment.kt
+++ app/src/main/java/com/example/bobmukjaku/MapListFragment.kt
@@ -7,10 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import com.example.bobmukjaku.Model.RestaurantList
-import com.example.bobmukjaku.R
-import com.example.bobmukjaku.RestaurantInfoDialog
+import com.example.bobmukjaku.*
 import com.example.bobmukjaku.databinding.FragmentMapListBinding
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
@@ -18,25 +17,24 @@ import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
-import com.naver.maps.map.overlay.InfoWindow
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.xmlpull.v1.XmlPullParser
-import org.xmlpull.v1.XmlPullParserFactory
-import java.net.HttpURLConnection
-import java.net.URL
 
 class MapListFragment : Fragment(), OnMapReadyCallback {
     private lateinit var mapView: MapView
     private val markerInfoWindowMap = mutableMapOf<Marker, DialogFragment>()
+    private lateinit var viewModel: MapListViewModel
 
     lateinit var binding: FragmentMapListBinding
     lateinit var mContext: Context
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         mContext = context
+        val repository = RestaurantRepository()
+        val viewModelFactory = MapListViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory)[MapListViewModel::class.java]
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -45,28 +43,20 @@ class MapListFragment : Fragment(), OnMapReadyCallback {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        mapView = view.findViewById(R.id.map_view)
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(this)
-    }
-
     override fun onMapReady(naverMap: NaverMap) {
         // 지도 디폴트 위치 고정
-//        naverMap.moveCamera(com.naver.maps.map.CameraUpdate.scrollTo(LatLng(37.54130009, 127.0701751)).animate(com.naver.maps.map.CameraAnimation.Easing))
         val cameraPosition = CameraPosition(
             LatLng(37.54130009, 127.0701751),  // 위치 지정
             16.0 // 줌 레벨
         )
         naverMap.cameraPosition = cameraPosition
 
-        // 코루틴에서 API 가져오기
         lifecycleScope.launch {
             val indsMclsCdList = listOf("I201", "I202", "I203", "I204", "I205", "I206", "I211")
 
             for (categoryList in indsMclsCdList) {
-                val restaurantList = restaurantApi(categoryList)
+                viewModel.fetchRestaurantList(categoryList)
+                val restaurantList = viewModel.restaurantList.value ?: emptyList()
 
                 val markerList = mutableListOf<Marker>()
 
@@ -117,59 +107,16 @@ class MapListFragment : Fragment(), OnMapReadyCallback {
                         }
                     }
                 }
-
                 naverMap.addOnCameraIdleListener(onCameraIdleListener)
             }
         }
     }
 
-    private suspend fun restaurantApi(categoryList: String): MutableList<RestaurantList> = withContext(Dispatchers.IO) {
-        val restaurantList = mutableListOf<RestaurantList>()
-
-        val serviceKey = "I%2BMzNcsHcMWL7gORiWo%2BBaZ%2FPl8w4OpluiaN88eg5zIYnjtoQ0pxS6Vpy6OaHBaIf%2BrZf9%2FgjDcrtUBv%2BcuhCw%3D%3D"
-        val pageNo = "&pageNo=1"
-        val numOfRows = "&numOfRows=300"
-        val dong = "&divId=adongCd"
-        val key = "&key=11215710" // 동단위 key
-        val indsLclsCd = "&indsLclsCd=I2" // 대분류
-        val indsMclsCd = "&indsMclsCd=$categoryList" // 중분류
-        val type = "&type=xml"
-        val url = "https://apis.data.go.kr/B553077/api/open/sdsc2/storeListInDong?serviceKey=" +
-                "$serviceKey$pageNo$numOfRows$dong$key$indsLclsCd$indsMclsCd$type"
-
-        val apiUrl = URL(url)
-        val connection = apiUrl.openConnection() as HttpURLConnection
-        val inputStream = connection.inputStream
-
-        // 음식점 데이터 파싱 및 리스트에 저장
-        val parser = XmlPullParserFactory.newInstance().newPullParser()
-        parser.setInput(inputStream, "UTF-8") // urlStream은 API 호출 결과 스트림
-
-        var eventType = parser.eventType
-        var currentRestaurant: RestaurantList? = null
-
-        while (eventType != XmlPullParser.END_DOCUMENT) {
-            when (eventType) {
-                XmlPullParser.START_TAG -> {
-                    when (parser.name) {
-                        "item" -> currentRestaurant = RestaurantList("", "", "", "", 0.0, 0.0)
-                        "bizesNm" -> currentRestaurant?.bizesNm = parser.nextText()
-                        "indsMclsNm" -> currentRestaurant?.indsMclsNm = parser.nextText()
-                        "indsSclsNm" -> currentRestaurant?.indsSclsNm = parser.nextText()
-                        "lnoAdr" -> currentRestaurant?.lnoAdr = parser.nextText()
-                        "lat" -> currentRestaurant?.lat = parser.nextText().toDouble()
-                        "lon" -> currentRestaurant?.lon = parser.nextText().toDouble()
-                    }
-                }
-                XmlPullParser.END_TAG -> {
-                    if (parser.name == "item" && currentRestaurant != null) {
-                        restaurantList += currentRestaurant
-                    }
-                }
-            }
-            eventType = parser.next()
-        }
-        return@withContext restaurantList
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView = view.findViewById(R.id.map_view)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
     }
 
     override fun onStart() {
---
+++ app/src/main/java/com/example/bobmukjaku/MapListViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.bobmukjaku
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.bobmukjaku.Model.RestaurantList
+
+class MapListViewModel(private val repository: RestaurantRepository) : ViewModel() {
+    // LiveData를 사용하여 음식점 리스트를 관리
+    private val _restaurantList = MutableLiveData<List<RestaurantList>>()
+    val restaurantList: LiveData<List<RestaurantList>> get() = _restaurantList
+
+    // UI에서 호출하는 함수로 음식점 정보를 가져옴
+    suspend fun fetchRestaurantList(categoryList: String) {
+        _restaurantList.value = repository.getRestaurantList(categoryList)
+    }
+}
