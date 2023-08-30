7c9f2a3a125097509e068c5c05c2445cd59d91a4
kimdm4638 <64524182+HdmkK@users.noreply.github.com>
2023-08-31 01:44:22 +0900

    리뷰 레이아웃 완성

--- app/src/main/java/com/example/bobmukjaku/Model/MemberService.kt
+++ app/src/main/java/com/example/bobmukjaku/Model/MemberService.kt
@@ -94,7 +94,7 @@ interface MemberService {
 
 object RetrofitClient {
     //private const val BASE_URL = "https://4a76-124-58-128-62.ngrok-free.app" // 여기에 서버 URL 넣기
-    private const val BASE_URL = "http://192.168.219.107:8080" // 여기에 서버 URL 넣기
+    private const val BASE_URL = "http://192.168.219.109:8080" // 여기에 서버 URL 넣기
 
     private fun provideOkHttpClient(interceptor: ContentTypeInterceptor): OkHttpClient
             = OkHttpClient.Builder().run {
