e69fbb325ce41ce198aecbf70948f982f0e9f538
jiheunkim <ziheun54@gmail.com>
2023-09-04 11:36:03 +0900

    리뷰 스토리지 코드 주석 추가

--- app/src/main/java/com/example/bobmukjaku/ScrapReviewListAdapter.kt
+++ app/src/main/java/com/example/bobmukjaku/ScrapReviewListAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.bobmukjaku
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bobmukjaku.Model.ReviewResponse
 import com.example.bobmukjaku.databinding.ReviewAllListBinding
+import com.google.firebase.ktx.Firebase
 
 class ScrapReviewListAdapter(var items: List<ReviewResponse>) : RecyclerView.Adapter<ScrapReviewListAdapter.ViewHolder>() {
 
@@ -31,7 +33,23 @@ class ScrapReviewListAdapter(var items: List<ReviewResponse>) : RecyclerView.Ada
         val reviewInfo = items[position]
 
         holder.binding.content.text = "→ " + reviewInfo.contents
-//        holder.binding.image = reviewInfo.imageUrl
+
+        // 파이어베이스 스토리지에서 이미지 다운로드 및 설정
+//        if (reviewInfo.imageUrl.isNotEmpty()) {
+//            val storageRef = Firebase.storage.reference.child(reviewInfo.imageUrl)
+//
+//            storageRef.downloadUrl.addOnSuccessListener { uri ->
+//                Glide.with(holder.itemView.context)
+//                    .load(uri)
+//                    .into(holder.binding.image) // Glide를 사용하여 이미지 설정
+//            }.addOnFailureListener {
+//                // 이미지 다운로드 실패 시 처리
+//                Log.e("ImageDownload", "Failed to download image: ${it.message}")
+//            }
+//        } else {
+//            // 이미지 주소가 없을 경우 처리
+//            holder.binding.image.setImageResource(R.drawable.default_image) // 기본 이미지 설정 또는 비어있는 경우 아무 작업하지 않음
+//        }
     }
 
     override fun getItemCount(): Int {
