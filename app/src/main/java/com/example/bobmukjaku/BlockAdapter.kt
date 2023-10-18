46d9228f2cd5a66e41981b5c33a6e814bdf2fabc
jiheunkim <ziheun54@gmail.com>
2023-10-18 14:50:48 +0900

    밥친구: 차단 목록 불러오기

---
+++ app/src/main/java/com/example/bobmukjaku/BlockAdapter.kt
@@ -0,0 +1,93 @@
+package com.example.bobmukjaku
+
+import android.annotation.SuppressLint
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bobmukjaku.Dto.BlockInfoDto
+import com.example.bobmukjaku.Dto.FriendInfoDto
+import com.example.bobmukjaku.Model.*
+import com.example.bobmukjaku.databinding.BlockListBinding
+import com.example.bobmukjaku.databinding.FriendListBinding
+
+class BlockAdapter(var items: List<BlockInfoDto>, var onBlockRemovedListener: OnBlockRemovedListener): RecyclerView.Adapter<BlockAdapter.ViewHolder>() {
+
+    interface OnItemClickListener{
+        fun onItemClick(pos: Int, friendInfo: FriendInfoDto)
+    }
+
+    var onItemClickListener:OnItemClickListener? = null
+
+    interface OnBlockRemovedListener {
+        fun onBlockRemoved(position: Int)
+    }
+
+    inner class ViewHolder(var binding: BlockListBinding): RecyclerView.ViewHolder(binding.root){
+    }
+
+    fun updateItems(newItems: List<BlockInfoDto>) {
+        items = newItems
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = BlockListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, @SuppressLint("RecyclerView") position: Int) {
+        val blockInfo = items[position]
+        val bgResourceId = when (blockInfo.blockProfileColor) {
+            "bg1" -> R.drawable.bg1
+            "bg2" -> R.drawable.bg2
+            "bg3" -> R.drawable.bg3
+            "bg4" -> R.drawable.bg4
+            "bg5" -> R.drawable.bg5
+            "bg6" -> R.drawable.bg6
+            "bg7" -> R.drawable.bg7
+            "bg8" -> R.drawable.bg8
+            "bg9" -> R.drawable.bg9
+            "bg10" -> R.drawable.bg10
+            "bg11" -> R.drawable.bg11
+            "bg12" -> R.drawable.bg12
+            "bg13" -> R.drawable.bg13
+            "bg14" -> R.drawable.bg14
+            "bg15" -> R.drawable.bg15
+            "bg16" -> R.drawable.bg16
+            "bg17" -> R.drawable.bg17
+            "bg18" -> R.drawable.bg18
+            // 다른 리소스에 대한 처리 추가
+            else -> R.drawable.bg1 // 디폴트 리소스 ID 또는 오류 처리 리소스 사용
+        }
+
+        holder.binding.imgProfileBg.setBackgroundResource(bgResourceId)
+        holder.binding.tvItemChattingName.text = blockInfo.blockNickname
+
+        var level = blockInfo.blockRate.toString().toInt()
+        if (level <= 20) {
+            holder.binding.level.text = "1"
+            holder.binding.imgProfile.setBackgroundResource(R.drawable.ku_1)
+        } else if (level <= 40) {
+            level -= 20
+            holder.binding.level.text = "2"
+            holder.binding.imgProfile.setBackgroundResource(R.drawable.ku_2)
+        } else if (level <= 60) {
+            level -= 40
+            holder.binding.level.text = "3"
+            holder.binding.imgProfile.setBackgroundResource(R.drawable.ku_3)
+        } else if (level <= 80) {
+            level -= 60
+            holder.binding.level.text = "4"
+            holder.binding.imgProfile.setBackgroundResource(R.drawable.ku_4)
+        } else {
+            level -= 80
+            holder.binding.level.text = "5"
+            holder.binding.imgProfile.setBackgroundResource(R.drawable.ku_5)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
--- app/src/main/java/com/example/bobmukjaku/BlockFragment.kt
+++ app/src/main/java/com/example/bobmukjaku/BlockFragment.kt
@@ -8,21 +8,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.example.bobmukjaku.Model.Member
-import com.example.bobmukjaku.Model.RetrofitClient
-import com.example.bobmukjaku.Model.SharedPreferences
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.bobmukjaku.Dto.BlockInfoDto
+import com.example.bobmukjaku.Dto.FriendInfoDto
+import com.example.bobmukjaku.Dto.FriendUpdateDto
+import com.example.bobmukjaku.Model.*
 import com.example.bobmukjaku.databinding.FragmentBlockBinding
+import com.example.bobmukjaku.databinding.FragmentFriendListBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-class BlockFragment : Fragment() {
+class BlockFragment : Fragment(), BlockAdapter.OnBlockRemovedListener {
 
     lateinit var mContext: Context
     lateinit var binding: FragmentBlockBinding
-
-    var uid: Long = 0
+    lateinit var adapter: BlockAdapter
+    var blockList = mutableListOf<BlockInfoDto>()
 
     private val friendService = RetrofitClient.friendService
     private val accessToken = SharedPreferences.getString("accessToken", "")
@@ -44,36 +47,71 @@ class BlockFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        getUid()
+        getBlockList()
     }
 
-    private fun getUid() {
-        val memberService = RetrofitClient.memberService
-
-        val call = accessToken?.let { memberService.selectOne(authorizationHeader) }
-        call?.enqueue(object : Callback<Member> {
-            override fun onResponse(call: Call<Member>, response: Response<Member>) {
+    // 친구 추가 테스트 코드
+    private fun addBlock() {
+        val friendInfo = FriendUpdateDto(friendUid = 3)
+        val call = friendService.blockFriend(authorizationHeader, friendInfo)
+        call.enqueue(object : Callback<Unit> {
+            override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
                 if (response.isSuccessful) {
-                    val member = response.body()
-                    val uidInfo = member?.uid
-                    if (uidInfo != null) {
-                        uid = uidInfo
-//                        getScrapList()
-                    }
+                    Log.i("RegisterFriend", "차단 등록 완료")
                 } else {
                     val errorCode = response.code()
-                    Toast.makeText(
-                        requireContext(),
-                        "uid를 가져오는데 실패했습니다. 에러 코드: $errorCode",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    Log.i("RegisterFriend", "차단 등록 실패 $errorCode")
                 }
             }
 
-            override fun onFailure(call: Call<Member>, t: Throwable) {
+            override fun onFailure(call: Call<Unit>, t: Throwable) {
                 // 네트워크 오류 또는 기타 에러가 발생했을 때의 처리
-                t.message?.let { it1 -> Log.i("[uid 로드 실패: ]", it1) }
+                t.message?.let { it1 -> Log.i("[차단 등록 기타 에러: ]", it1) }
             }
         })
+    }
+    private fun getBlockList() {
+        val call = friendService.getBlockList(authorizationHeader)
+
+        call.enqueue(object : Callback<List<BlockInfoDto>> {
+            override fun onResponse(call: Call<List<BlockInfoDto>>, response: Response<List<BlockInfoDto>>) {
+                if (response.isSuccessful) {
+                    val blockListResponse = response.body() // 서버에서 받은 친구 목록
+                    if (blockListResponse != null) {
+                        blockList.clear()
+                        blockList.addAll(blockListResponse) // friendList에 업데이트된 친구 목록 저장
+                        adapter.updateItems(blockList) // 어댑터에 업데이트된 목록 전달
+
+                        binding.totalBlock.text = blockList.size.toString()
+                    }
+                    val successCode = response.code()
+                    Log.i("내 차단 목록 로드", "성공 $successCode")
+                } else {
+                    val errorCode = response.code()
+                    Log.i("내 차단 목록 로드", "에러 $errorCode")
+                }
+            }
+
+            override fun onFailure(call: Call<List<BlockInfoDto>>, t: Throwable) {
+                // 네트워크 오류 또는 기타 에러가 발생했을 때의 처리
+                t.message?.let { it1 -> Log.i("[내 차단 목록 로드 에러: ]", it1) }
+            }
+        })
+
+        binding.blockRecyclerView.layoutManager = LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false)
+        adapter = BlockAdapter(blockList, this@BlockFragment)
+        adapter.onItemClickListener = object : BlockAdapter.OnItemClickListener {
+            override fun onItemClick(pos: Int, friendInfo: FriendInfoDto) {
+
+            }
+        }
+        binding.blockRecyclerView.adapter = adapter
+    }
+
+
+    override fun onBlockRemoved(position: Int) {
+        blockList.removeAt(position)
+        binding.totalBlock.text = blockList.size.toString()
+        adapter.notifyDataSetChanged()
     }
 }
