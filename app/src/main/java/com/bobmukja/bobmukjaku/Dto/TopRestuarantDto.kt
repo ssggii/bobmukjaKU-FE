5522350773410e62215adfc7372a8f479752cf32
jiheunkim <ziheun54@gmail.com>
2023-11-25 11:03:37 +0900

    맛지도: Top 음식점 반환 data 수정

--- app/src/main/java/com/bobmukja/bobmukjaku/Dto/TopRestuarantDto.kt
+++ app/src/main/java/com/bobmukja/bobmukjaku/Dto/TopRestuarantDto.kt
@@ -4,6 +4,4 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class TopRestuarantDto(
     @JsonProperty("placeId")val placeId: String,
-    @JsonProperty("placeName")val placeName: String,
-    @JsonProperty("scrapCount")val scrapCount: Int
     )
